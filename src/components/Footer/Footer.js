131e274028a5607c071fa927cf0c0327bb46ac4c
dylman123 <dylanklein29@gmail.com>
2022-02-17 11:04:11 +1100

    fixed footer link styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,16 +5,28 @@ import './Footer.css';
 export default function Footer() {
   return (
     <footer className='footer'>
-      <a className='footerHeading' href='/'>
-        <Logo />
-      </a>
-      <p>
-        ANKC registered breeders educating the wider community.
-      </p>
+      <div className='footerLinkContainer'>
+        <a className='footerHeading' href='/'>
+          <Logo />
+        </a>
+        <p>
+          ANKC registered breeders educating the wider community.
+        </p>
+        <div className='ctaFooter'>
+          <a className='ctaPrimary' href='/#join'>
+            Become an author
+          </a>
+        </div>
+      </div>
+      <div className='footerLinkContainer'>
+        <a className='footerLink' href ='/learning-centre'>
+          <p>Learning Centre</p>
+        </a>
+        <a className='footerLink' href='/help'>
+          <p>Help</p>
+        </a>
+      </div>
 
-      <a className='ctaPrimary' href='/#join'>
-        Become an author
-      </a>
     </footer>
   );
 }
