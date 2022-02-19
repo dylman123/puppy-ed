0039b1c6ddc5b5a53258216af63804e1bdb56580
dylman123 <dylanklein29@gmail.com>
2022-02-19 19:48:35 +1100

    added module details to module page

--- src/containers/LearnerModulePage/LearnerModulePage.js
+++ src/containers/LearnerModulePage/LearnerModulePage.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { modules } from '../../assets/learner-modules/modules';
 import { useParams } from "react-router-dom";
+
+import './LearnerModulePage.css';
 
 export default function LearnerModulePage() {
     let { slug } = useParams();
 
+    const currentModule = modules.find(m => m.slug === slug);
+
     return (
         <div className='learnerModulePage'>
-            {slug}
+            <h2 className='moduleTitle'>{currentModule.title}</h2>
+            <img className='moduleIcon' src={currentModule.imageFile} />
+            <p className='moduleDescription'>{currentModule.description}</p>
         </div>
     );
 }
