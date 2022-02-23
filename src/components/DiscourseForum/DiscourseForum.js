ddf03b69151ce0fe42d420ec53f75db61b9603f0
dylman123 <dylanklein29@gmail.com>
2022-02-23 21:53:31 +1100

    syntax4

--- src/components/DiscourseForum/DiscourseForum.js
+++ src/components/DiscourseForum/DiscourseForum.js
@@ -17,7 +17,7 @@ export default function DiscourseForum(topicId) {
         d.async = true;
         d.src = window.DiscourseEmbed.discourseUrl + 'javascripts/embed.js';
         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d);
-    }, [topicId]);
+    }, [id]);
 
     return (
         <div>
