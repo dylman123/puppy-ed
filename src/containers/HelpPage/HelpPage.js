98ce48384e918f4e7d063400f846bfb243275654
dylman123 <dylanklein29@gmail.com>
2022-03-07 17:26:11 +1100

    added help page content

--- src/containers/HelpPage/HelpPage.js
+++ src/containers/HelpPage/HelpPage.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import './HelpPage.css'
+import image from './reachout.svg';
 
 export default function HelpPage() {
   return (
-      <div className='helpPage'>
+    <div className='helpPage'>
+      <h1 className='pageTitle'>Contact us</h1>
+      <img className='coverImage' src={image} alt="Reach out" />
 
+      <div className='contentMain'>
+        <p className='contentOpener'>Please don't hesitate to reach out!</p>
+        <p className='subText'>For any questions, comments, feedback or just to say hi, contact the Puppy Ed team.</p>
+        <div>
+          <h2 className='contactMethod'>
+            Email us
+          </h2>
+          <a href="mailto:support@doggydoor.com">
+            <p className='contactMethod'>
+              support@doggydoor.com
+            </p>
+          </a>
+        </div>
       </div>
+    </div>
   );
 }
